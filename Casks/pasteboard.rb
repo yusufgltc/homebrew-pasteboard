e4df603f4ad4ac9067f9123d86ff9a7f14cdcd8d
Yusuf Gultac <yusufgultac@Yusufs-MacBook-Pro.local>
2026-06-30 22:26:24 +0200

    Update SHA256 and add postflight auto-launch

--- Casks/pasteboard.rb
+++ Casks/pasteboard.rb
@@ -1,6 +1,6 @@
 cask "pasteboard" do
   version "1.0.0"
-  sha256 "7aaea61c2de703872453b010c23a7620bc01548d62d5f50de53f367320801b19"
+  sha256 "1baee56c3c3a11b22d609305c08b7713f8275c979a399dc3bf805ac658f8ad17"
 
   url "https://github.com/yusufgltc/PasteBoard/releases/download/v#{version}/PasteBoard-#{version}.zip"
   name "PasteBoard"
@@ -8,6 +8,10 @@ cask "pasteboard" do
   homepage "https://github.com/yusufgltc/PasteBoard"
 
   app "PasteBoard.app"
+
+  postflight do
+    system_command "/usr/bin/open", args: ["-a", "PasteBoard"]
+  end
 
   zap trash: [
     "~/Library/Application Support/PasteBoard",
