5ff7556e77cad010db9abb5978e3d00914237431
Yusuf Gultac <yusufgultac@Yusufs-MacBook-Pro.local>
2026-06-30 21:54:55 +0200

    Fix GitHub username in cask URL (ygultac → yusufgltc)

--- Casks/pasteboard.rb
+++ Casks/pasteboard.rb
@@ -2,10 +2,10 @@ cask "pasteboard" do
   version "1.0.0"
   sha256 "7aaea61c2de703872453b010c23a7620bc01548d62d5f50de53f367320801b19"
 
-  url "https://github.com/ygultac/PasteBoard/releases/download/v#{version}/PasteBoard-#{version}.zip"
+  url "https://github.com/yusufgltc/PasteBoard/releases/download/v#{version}/PasteBoard-#{version}.zip"
   name "PasteBoard"
   desc "Clipboard history manager for macOS"
-  homepage "https://github.com/ygultac/PasteBoard"
+  homepage "https://github.com/yusufgltc/PasteBoard"
 
   app "PasteBoard.app"
 
